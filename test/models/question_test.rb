4a3e7322cdaf90b9ec78257c6e18a915b7247718
euge <eugeniohernandez17@gmail.com>
2016-02-26 17:52:18 -0430

    The test classes of the new models and the first tests of the model question.

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid count tags of question" do
+  	question = Question.find(1)
+  	assert_equal question.tags.count, 1
+  end
+  
+  test "valid delete tags of question" do
+  	question = Question.find(1)
+  	question.tags.delete(1)
+  	assert_equal question.tags.count, 0
+  	assert_equal 2, Tag.count
+  end
 end
