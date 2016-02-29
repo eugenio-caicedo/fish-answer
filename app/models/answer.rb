bfeab04822e45e29fccc885c89e4deba2d4e94f0
euge <eugeniohernandez17@gmail.com>
2016-02-29 18:26:27 -0430

    The client's info was visualize in the Answer view.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,18 @@ class Answer < ActiveRecord::Base
 	
 	scope :paginable, ->(page, limit){ paginate(:page => page, :per_page => limit) }
 	scope :by_question, ->(question){ includes(:client).where(question: question) }
+	
+	#METODOS PROPIOS DE LA CLASE
+	def createdAtFormated
+		dateFormat created_at
+	end
+	
+	def updatedAtFormated
+		dateFormat updated_at
+	end
+	
+private
+	def dateFormat(date)
+		date.strftime "%d-%m-%Y a las %H:%M"
+	end
 end
